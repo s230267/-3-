dffa9a20899754891e7401fe89e7bf1430a123ae
s230267 <gimpo.s230267@ggh.goe.go.kr>
2024-11-26 16:08:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ with col1:
       st.subheader('모델 설명 ')
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
       st.write(' - 학습 데이터 출처 : https://data.kma.go.kr/data/weatherIssue/slthtList.do?pgmNo=690')
-      st.write(' - 훈련    데이터 : 700건')
-      st.write(' - 테스트 데이터 : 300건')
+      st.write(' - 훈련    데이터 : 3321건')
+      st.write(' - 테스트 데이터 : 1424건')
       st.write(' - 모델 정확도 :0.97 ')
 
 # 3. 데이터시각화
